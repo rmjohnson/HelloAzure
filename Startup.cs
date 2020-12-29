a4ce550eea0498225033951e8740364d971d509c
Ryan Johnson <ryan.johnson@logos.com>
2020-12-29 16:20:21 -0600

    CD test cycle time

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace HelloAzure
 			{
 				endpoints.MapGet("/", async context =>
 				{
-					await context.Response.WriteAsync("Hello Azure!");
+					await context.Response.WriteAsync("Hello Azure! Hello CD!");
 				});
 			});
 		}
