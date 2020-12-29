dabc8879923eacf27633655d54221564e6113f1d
Ryan Johnson <ryan.johnson@logos.com>
2020-12-29 15:30:06 -0600

    Test out CD

--- Startup.cs
+++ Startup.cs
@@ -9,26 +9,26 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace HelloAzure
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello Azure!");
-            });
-        }
-    }
+			app.Run(async (context) =>
+			{
+				await context.Response.WriteAsync("Hello Azure! Hello CD!");
+			});
+		}
+	}
 }
